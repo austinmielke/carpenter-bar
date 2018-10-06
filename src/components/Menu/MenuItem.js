cef07ab344e986e92043b0b93b91d9f8b2ecc99b
Austin Mielke <amielkedev@gmail.com>
2018-10-06 14:01:05 -0500

    Tweaked menu component styles

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -3,9 +3,11 @@ import React from "react";
 const MenuItem = () => (
   <div className="w-full sm:w-1/2 md:w-1/3 mx-auto py-4 text-center sm:text-left text-gold">
     <h3 className="py-2">
-      Toulouse <span>$9</span>
+      Toulouse <span className="ml-4 font-normal">$9</span>
     </h3>
-    <p>Reposado Patron | Angostura Bitters | Pompelo Soda | Mint</p>
+    <p className="italic">
+      Reposado Patron | Angostura Bitters | Pompelo Soda | Mint
+    </p>
   </div>
 );
 
