b759751ef64d5c53b242dfa215f72fe300f0f96e
Austin Mielke <amielkedev@gmail.com>
2018-10-07 17:26:53 -0500

    Tweaked header logosize for responsiveness

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,11 @@ const Header = () => (
     className="h-screen w-full bg-black flex flex-col items-center justify-center"
   >
     <NavBar />
-    <img className="w-1/2 flex-1" src={logoLarge} alt="Carpenter Bar" />
+    <img
+      className="w-3/4 md:w-1/2 flex-1"
+      src={logoLarge}
+      alt="Carpenter Bar"
+    />
     <img className="arrow w-8 mb-8" src={arrow} alt="scroll down" />
   </header>
 );
