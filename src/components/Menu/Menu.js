53d3bcc8f4383caf50ab7fb5edccc03ec9fc77b9
Austin Mielke <amielkedev@gmail.com>
2018-10-17 20:27:30 -0500

    Removed MenuItem component to test CMS

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,17 +1,12 @@
 import React from "react";
 import MenuItem from "./MenuItem";
 
-const Menu = ({ menu }) => (
+const Menu = () => (
   <section id="menu" className="my-32">
     <div className="container mx-auto text-gold">
       <h2 className="pb-8 mb-8 border-gold border-b-4 text-center text-4xl uppercase tracking-wide">
         Signature Cocktails
       </h2>
-      <MenuItem
-        name={menu.markdownRemark.frontmatter.title}
-        price={menu.markdownRemark.frontmatter.price}
-        desc={menu.markdownRemark.frontmatter.description}
-      />
     </div>
   </section>
 );
